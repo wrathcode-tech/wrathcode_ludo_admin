473cdad0b0cbabe61725cd0cba13fc19bd70cef9
Rajesh <rajesh@wrathcode.com>
2025-10-28 13:00:19 +0530

    ui updates

--- src/Components/DisputeResponse/DisputeResponse.js
+++ src/Components/DisputeResponse/DisputeResponse.js
@@ -198,7 +198,9 @@ function DisputeResponse() {
                                     </p>
 
                                     <p><strong>Joined By:</strong> {selectedWinningData.joinedBy || "—"}</p>
-                                    <p><strong>Total Amount:</strong> ₹{selectedWinningData.amount || 0}</p>
+                                    <p>
+                                        <strong>Total Amount:</strong> ₹{(selectedWinningData?.amount ?? 0) * 2}
+                                    </p>
 
                                 </div>
                             </div>
